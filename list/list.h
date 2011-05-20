4477ea80c83a36f83d913963fa2912d1bd6e5529
John Reese <john@noswap.com>
2011-05-19 20:31:37 -0400

    Initial templated implementation of linked list and iterator

--- list/list.h
+++ list/list.h
@@ -3,39 +3,92 @@
  * Licensed under the MIT license
  */
 
+#include <stdlib.h>
+
 /**
  * List node.
  */
 template <class T>
-class ListNode {
+class ListNode
+{
 	public:
 
-		ListNode();
+		/**
+		 * Create an unconnected node containing the given value.
+		 *
+		 * @param Element to contain
+		 */
+		ListNode(T element)
+		{
+			next = NULL;
+			prev = NULL;
+			value = element;
+		}
 
 		ListNode<T>* next;
 		ListNode<T>* prev;
-		T element;
+		T value;
 
 };
 
+template <class T>
+class ListIter;
+
 /**
- * Linked list structure.
+ * Linked list implementation using doubly-linked nodes.
  */
 template <class T>
-class List {
+class List
+{
 	public:
 
 		/**
 		 * Create a new, empty list.
 		 */
-		List();
+		List()
+		{
+			count = 0;
+			start = NULL;
+			end = NULL;
+		}
 
 		/**
 		 * Number of elements currently in the list.
 		 *
 		 * @return List length
 		 */
-		int length();
+		int length()
+		{
+			return count;
+		}
+
+		/**
+		 * Find the position of the element with the given value.
+		 *
+		 * @param Element to search for
+		 * @return Position of element, or -1 if not found
+		 */
+		int find(T element)
+		{
+			if (start == NULL)
+			{
+				return -1;
+			}
+
+			ListNode<T>* iter = start;
+
+			for (unsigned int i = 0; i < count; i++)
+			{
+				if (iter->value == element)
+				{
+					return i;
+				}
+
+				iter = iter->next;
+			}
+
+			return -1;
+		}
 
 		/**
 		 * Retrieve an element at the given position.
@@ -43,14 +96,47 @@ class List {
 		 * @param Position of the requested element
 		 * @return Requested element, or null if not found
 		 */
-		T get(int);
+		T get(unsigned int position)
+		{
+			ListNode<T>* iter = start;
+
+			if (position >= count)
+			{
+				return (T) NULL;
+			}
+
+			for (unsigned int i = 0; i < position; i++)
+			{
+				iter = iter->next;
+			}
+
+			return iter->value;
+		}
 
 		/**
 		 * Insert an element into the list.
 		 *
 		 * @param Element to insert
 		 */
-		void insert(T);
+		void append(T element)
+		{
+			ListNode<T>* iter = new ListNode<T>(element);
+
+			iter->prev = end;
+
+			if (start == NULL)
+			{
+				start = iter;
+			}
+			if (end != NULL)
+			{
+				end->next = iter;
+			}
+
+			end = iter;
+
+			count++;
+		}
 
 		/**
 		 * Insert an element into the list at a given position.
@@ -58,13 +144,224 @@ class List {
 		 * @param Element to insert
 		 * @param Position to insert element at
 		 */
-		void insert(T, int);
+		void insert(T element, unsigned int position)
+		{
+			if (position >= count)
+			{
+				return append(element);
+			}
 
-	private:
+			ListNode<T>* node = new ListNode<T>(element);
+			ListNode<T>* iter = start;
+			ListNode<T>* last = NULL;
 
-		int count;
-		
-		ListNode<T>* list;
+			for (unsigned int i = 0; i < position; i++)
+			{
+				if (iter->next == NULL)
+				{
+					break;
+				}
+				else
+				{
+					last = iter;
+					iter = iter->next;
+				}
+			}
+
+			if (last == NULL)
+			{
+				start = node;
+			}
+			else
+			{
+				last->next = node;
+			}
+
+			node->prev = iter->prev;
+			iter->prev = node;
+			node->next = iter;
+
+			count++;
+		}
+
+		/**
+		 * Remove an element from the list at a given position.
+		 *
+		 * @param Position to remove
+		 * @return Removed element
+		 */
+		T remove(unsigned int position)
+		{
+			ListNode<T>* iter = start;
+			ListNode<T>* last = NULL;
+
+			if (position >= count)
+			{
+				return (T) NULL;
+			}
+
+			for (unsigned int i = 0; i < position; i++)
+			{
+				if (iter->next == NULL)
+				{
+					break;
+				}
+				else
+				{
+					last = iter;
+					iter = iter->next;
+				}
+			}
+
+			T value = iter->value;
+
+			if (last != NULL)
+			{
+				last->next = iter->next;
+			}
+
+			if (iter->next == NULL)
+			{
+				end = last;
+			}
+			else
+			{
+				iter->next->prev = last;
+			}
+
+			count--;
+			delete iter;
+
+			return value;
+		}
+
+		ListNode<T>* start;
+		ListNode<T>* end;
+
+	protected:
+
+		unsigned int count;
 		
 };
 
+/**
+ * List iterator.
+ */
+template <class T>
+class ListIter
+{
+	public:
+
+		/**
+		 * Create a new iterator.
+		 */
+		ListIter()
+		{
+			position = NULL;
+		}
+
+		/**
+		 * Create a new iterator pointing at the beginning of the given list.
+		 */
+		ListIter(List<T>& l)
+		{
+			position = l.start;
+		}
+
+		/**
+		 * Set an iterator position to the given list node.
+		 *
+		 * @param Target node
+		 */
+		ListIter<T> operator=(const ListNode<T>* node)
+		{
+			position = node;
+			return *this;
+		}
+
+		/**
+		 * Get the current element pointed to.
+		 *
+		 * @return Current element
+		 */
+		T value()
+		{
+			if (position == NULL)
+			{
+				return (T) NULL;
+			}
+
+			return position->value;
+		}
+
+		/**
+		 * Return the current element when dereferenced.
+		 *
+		 * @return Current element
+		 */
+		T operator*()
+		{
+			if (position == NULL)
+			{
+				return (T) NULL;
+			}
+			
+			return position->value;
+		}
+
+		/**
+		 * Prefix increment the iterator to the next list node.
+		 */
+		void operator++()
+		{
+			if (position != NULL)
+			{
+				position = position->next;
+			}
+		}
+
+		/**
+		 * Postfix increment the iterator to the next list node.
+		 *
+		 * @return Iterator pointing to the previous node
+		 */
+		ListIter<T> operator++(int)
+		{
+			ListIter<T> old;
+			old.position = position;
+
+			if (position != NULL)
+			{
+				position = position->next;
+			}
+
+			return old;
+		}
+
+		/**
+		 * Check if the iterator points at the given node.
+		 *
+		 * @param Target node
+		 * @return True if pointing at target
+		 */
+		bool operator==(ListNode<T>* node)
+		{
+			return position == node;
+		}
+
+		/**
+		 * Check if the iterator points at the given node.
+		 *
+		 * @param Target node
+		 * @return False if pointing at target
+		 */
+		bool operator!=(ListNode<T>* node)
+		{
+			return position != node;
+		}
+
+	private:
+
+		ListNode<T>* position;
+
+};
--- list/test.cpp
+++ list/test.cpp
@@ -3,12 +3,96 @@
 
 #include "list.h"
 
+/**
+ * Print a List of integers.
+ *
+ * @param List to print
+ */
+void print(List<int>& l)
+{
+	printf("\nprint()... ");
+
+	ListIter<int> i(l);
+
+	if (i == NULL)
+	{
+		printf("empty");
+	}
+	else
+	{
+		printf("%d", *i);
+		++i;
+	}
+
+	for (; i != NULL; ++i)
+	{
+		printf(", %d", *i);
+	}
+
+	printf("\n\n");
+}
+
+/**
+ * Test populating, searching, and removing integers from a List.
+ *
+ * @param Argument count
+ * @param Argument list
+ * @return Error level
+ */
 int main(int argc, char* argv[])
 {
-	printf("Creating list...\n");
+	List<int> l;
+	printf("List created: length: %d\n", l.length());
 
-	List<void*> l;
-	printf("Length: %d\n", l.length());
+	print(l);
+
+	// Test append() and insert()
+	l.append(1);
+	printf("append(1): length: %d\n", l.length());
+
+	l.append(2);
+	printf("append(2): length: %d\n", l.length());
+
+	l.insert(3,1);
+	printf("insert(3,1): length: %d\n", l.length());
+
+	l.insert(4,0);
+	printf("insert(4,0): length: %d\n", l.length());
+
+	l.append(5);
+	printf("append(5): length: %d\n", l.length());
+
+	l.insert(6,7);
+	printf("insert(6,7): length: %d\n", l.length());
+
+	print(l);
+
+	// Test find()
+	int pos = l.find(4);
+	printf("find(4): position: %d\n", pos);
+
+	pos = l.find(5);
+	printf("find(5): position: %d\n", pos);
+
+	pos = l.find(6);
+	printf("find(6): position: %d\n", pos);
+
+	// Test remove()
+	int pop = l.remove(1);
+	printf("remove(1): popped %d, length: %d\n", pop, l.length());
+
+	print(l);
+
+	pop = l.remove(3);
+	printf("remove(3): popped %d, length: %d\n", pop, l.length());
+
+	print(l);
+
+	pos = l.find(5);
+	printf("find(5): position: %d\n", pos);
+
+	pos = l.find(6);
+	printf("find(6): position: %d\n", pos);
 
 	return 0;
 }
